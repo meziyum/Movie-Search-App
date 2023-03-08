a2e480894fc1f450f4489831cc8abd6c3686e155
meziyum <siddhant24feb@gmail.com>
2023-03-08 16:00:13 +0530

    Update searchMovies.js

--- searchMovies.js
+++ searchMovies.js
@@ -9,10 +9,8 @@ export default function SearchMovies(){
     
     const searchMovies = async (e) => {
         e.preventDefault();
-
-        let API_KEY = 'YOUR API KEY HERE';
             
-        fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`)
+        fetch(`https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`)
         .then( res => res.json())
         .then( data => {
             setMovies(data.results);
